8ebf89ab2f450879fe9f18f51f9e8af3eec47ee4
Parviz Khavari <csctclan@gmail.com>
2018-09-30 18:16:34 +0300

    Added all http methods requests. Moved to Gatling 2.3.1

--- src/gatling/scala/test/TestSimulation.scala
+++ src/gatling/scala/test/TestSimulation.scala
@@ -5,20 +5,32 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
 class TestSimulation extends Simulation {
-    val httpConf = http.baseUrl("https://httpbin.org")
+  val httpConf = http.baseURL("https://httpbin.org")
 
-    val httpMethods = exec(
-        http("Request GET")
-            .get("/get")
+  val httpMethods = exec(
+    http("Request GET")
+      .get("/get")
+  ).exec(
+    http("Request POST")
+      .post("/post")
+  ).exec(
+    http("Request PUT")
+      .put("/put")
+  ).exec(
+    http("Request PATCH")
+      .patch("/patch")
+  ).exec(
+    http("Request DELETE")
+      .delete("/delete")
+  )
+
+  val scn = scenario("BasicSimulation")
+    .exec(
+      httpMethods
     )
+    .pause(5)
 
-    val scn = scenario("BasicSimulation")
-        .exec(
-            httpMethods
-        )
-        .pause(5)
-
-    setUp(
-        scn.inject(atOnceUsers(1))
-    ).protocols(httpConf)
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
 }
